fe849854f0ea4b76f6245213b7b109e0f9ea7394
Vyach23 <vyach1991@gmail.com>
2022-03-11 13:13:42 +0300

    added domain layer: UseCases Repository

---
+++ app/src/main/java/com/demo/shoplist/domain/GetShopListUseCase.kt
@@ -0,0 +1,10 @@
+package com.demo.shoplist.domain
+
+class GetShopListUseCase (private val shopListRepository: ShopListRepository) {
+
+    fun getShopList() : List<ShopItem> {
+       return shopListRepository.getShopList()
+    }
+
+
+}
