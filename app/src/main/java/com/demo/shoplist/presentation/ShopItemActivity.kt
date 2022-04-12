8849e7e2c44f4fe00aecfcf3827ac5b2846d4969
Vyach23 <vyach1991@gmail.com>
2022-04-12 13:29:46 +0300

    Added second layout Edited manifest for change screen

---
+++ app/src/main/java/com/demo/shoplist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.demo.shoplist.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.demo.shoplist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
